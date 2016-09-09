e65b843b93daa7f8bee38862923491427ca399e4
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-09-09 11:33:23 -0400

    add getters and setters for phone number and address, add html to twig template that displays new contact

--- src/Contact.php
+++ src/Contact.php
@@ -39,6 +39,27 @@
             return $this->first_name . " " . $this->last_name;
         }
 
+        function setPhone ($new_phone)
+        {
+            $this->phone = (int) $new_phone;
+        }
+
+        function getPhone()
+        {
+            return $this->phone;
+        }
+
+        function setAddress($new_address)
+        {
+            $this->address = (string) $new_address;
+        }
+
+        function getAddress()
+        {
+            return $this->address;
+        }
+
+
         function save()
         {
             array_push($_SESSION['list_of_contacts'], $this);
